b02b30606ceacb7eeef8788c0fb112495198edce
ZainabALbimani <42896884+ZainabALbimani@users.noreply.github.com>
2018-10-28 16:30:57 +0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,10 @@
-// variables - element,height, and width
+/// variables - element,height, and width
 const table = document.getElementById('pixelCanvas');
-let heightt = $("#inputHeight");
-let widthh = $("#inputWidth");
+let gridHeight = $("#inputHeight");
+let gridWidth = $("#inputWidth");
 
-$('#inputSubmit').click(function(Submit) {
-  Submit.preventDefault();
+$('#submitQuery').click(function(submit) {
+  submit.preventDefault();
   makeGrid();
 });
 
@@ -14,19 +14,19 @@ let color = $("#colorPicker");
 function makeGrid() {
 // Your code goes here!
     table.innerHTML = '';
-    let height = heightt.value();
-    let width = widthh.value();
+    let height = gridHeight.val();
+    let width = gridWidth.val();
 
     let addEvent = function(cell) {
         cell.addEventListener('click', function() {
-            cell.style.backgroundColor = color.value();
+            cell.style.backgroundColor = color.val();
         });
     }
 
     for (let x = 0; x < height; x++) {
-        let row = table.insertRow(i);
+        let row = table.insertRow(x);
         for (let y = 0; y < width; y++) {
-            let cell = row.insertCell(j);
+            let cell = row.insertCell(y);
             cell.addEventListener('click', addEvent(cell));
         }
     }
